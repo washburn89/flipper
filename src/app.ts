084a5fad60273a8e7be4a6216b171437de90e7a1
William.Washburn <william.l.washburn.ctr@mail.mil>
2022-02-10 14:25:10 -0600

    flip a coin

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
+import RandomOrg from 'random-org';
+
 export class App {
-  public message = 'Hello MTG!';
+  public random = new RandomOrg({ apiKey: 'ab5f7285-94f1-4c60-8b1d-a8d4a343745b' });
+
+
+  private async singleCoinFlip(){
+    let boolresult;
+    let stringresult: string = 'no coin flipped';
+
+    boolresult = await this.random.generateIntegers({n: 1, min: 0, max: 1})
+
+    if (boolresult = 0){
+      stringresult = 'tails'
+    }else{
+      stringresult = 'heads'
+    }
+  }
 }
